9b9e74b6f059f36c000e08bd4cad9136c0129318
Maria Grimaldi <maria.grimaldi@edunext.co>
2025-02-13 18:16:10 +0100

    refactor: address docs reviews

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
